38c1a2050fe47debbd4b46fca67a4d1f2a70efe5
Mary Tease <58113541+mary-ts@users.noreply.github.com>
2021-03-07 17:37:44 +0000

    Update Races.java

--- Races.java
+++ Races.java
@@ -2,13 +2,14 @@ public class Races {
 
 	String race;
 	String size;
+	int[] ability;
 	String[] langs;
 	boolean darkvision;
 	int speed;
 //	langs x;
 	
 	
-	public Races(String race, int speed, boolean darkvision, String size, String[] langs) {
+	public Races(String race, int[] ability, int speed, boolean darkvision, String size, String[] langs) {
 		this.race = race;
 		this.speed = speed;
 		this.darkvision = darkvision;
