8ac23bcc89188242aee3cc0c4b53d1a3b826e3df
ishalazo <58404081+ishalazo@users.noreply.github.com>
2021-03-07 16:08:56 +0000

    Add files via upload

--- playerChar.java
+++ playerChar.java
@@ -1,135 +1,141 @@
+import java.util.Scanner;
+
 public class playerChar extends Character{
 
     //String[] race = {"Dragonborn", "Dwarf", "Elf", "Gnome", "Half-Elf", "Halfling", "Half-Orc", "Human", "Tiefling"};
     //String[] classes = {Barbarian, Bard, Cleric, Druid, Fighter, Monk, Paladin, Ranger, Rogue, Sorcerer, Warlock, Wizard};
-    String playName;
-    String playRace;
-    Classes playClass;
+    private String playName;
+    private String playRace;
+    private Classes playClass;
+    private int[] generatedStats;
 
-    int prof = 2;
-    int dex;
-    int str;
-    int con;
-    int intel;
-    int wis;
-    int cha;
+    private int profBonus;
+    private int dex;
+    private int str;
+    private int con;
+    private int intel;
+    private int wis;
+    private int cha;
 
-    public playerChar(String name, String race, String Class){
-        this.playName = name;
-        this.playRace = race;
+    public playerChar(String playName, String playRace, int[] generatedStats, int profBonus) {
+		this.playName = playName;
+		this.playRace = playRace;
+		this.generatedStats = generatedStats;
+		this.profBonus = profBonus;
+	}
+    
+	public playerChar(String playName, String playRace, int profBonus, int dex, int str, int con, int intel, int wis,
+			int cha) {
+		this.playName = playName;
+		this.playRace = playRace;
+		this.profBonus = profBonus;
+		this.dex = dex;
+		this.str = str;
+		this.con = con;
+		this.intel = intel;
+		this.wis = wis;
+		this.cha = cha;
+	}
 
 
-        calcStats();
 
+	public String getPlayName() {
+		return playName;
+	}
+
+	public void setPlayName(String playName) {
+		this.playName = playName;
+	}
+
+	public String getPlayRace() {
+		return playRace;
+	}
+
+	public void setPlayRace(String playRace) {
+		this.playRace = playRace;
+	}
+
+	public int[] getGeneratedStats() {
+		return generatedStats;
+	}
+
+	public void setGeneratedStats(int[] generatedStats) {
+		this.generatedStats = generatedStats;
+	}
+
+	public int getProfBonus() {
+		return profBonus;
+	}
+
+	public void setProfBonus(int profBonus) {
+		this.profBonus = profBonus;
+	}
+
+	public int getDex() {
+		return dex;
+	}
+
+	public void setDex(int dex) {
+		this.dex = dex;
+	}
+
+	public int getStr() {
+		return str;
+	}
+
+	public void setStr(int str) {
+		this.str = str;
+	}
+
+	public int getCon() {
+		return con;
+	}
+
+	public void setCon(int con) {
+		this.con = con;
+	}
+
+	public int getIntel() {
+		return intel;
+	}
+
+	public void setIntel(int intel) {
+		this.intel = intel;
+	}
+
+	public int getWis() {
+		return wis;
+	}
+
+	public void setWis(int wis) {
+		this.wis = wis;
+	}
+
+	public int getCha() {
+		return cha;
+	}
+
+	public void setCha(int cha) {
+		this.cha = cha;
+	}
+
+	public void genStats() {
+    	generatedStats = new int[] {rollDice.sum3(),rollDice.sum3(),rollDice.sum3(),rollDice.sum3(),rollDice.sum3(),rollDice.sum3()};
+		System.out.println("Generated: ");
+        for(int i = 0; i < generatedStats.length; i ++) {			
+			System.out.println("[" + i + "] " + generatedStats[i]);
+		}
+		
     }
 
-    public playerChar(String name, String race, String Class, int d, int s, int c, int i, int w, int ch){
-        this.playName = name;
-        this.playRace = race;
-        this.dex = d;
-        this.str = s;
-        this.con = c;
-        this.intel = i;
-        this.wis = w;
-        this.cha = ch;
-
+    public void assignStats(String stat, int index){
+    	switch(stat) {
+    		case "Strength": str = generatedStats[index]; 
+    		case "Dexterity": dex = generatedStats[index]; 
+    		case "Constitution": con = generatedStats[index]; 
+    		case "Intelligence": intel = generatedStats[index]; 
+    		case "Wisdom": wis = generatedStats[index]; 
+    		case "Charisma": cha = generatedStats[index]; 
+    	}
     }
-
-    public int getProf() {
-        return prof;
-    }
-
-    public void setProf(int prof) {
-        this.prof = prof;
-    }
-
-    public String getPlayName() {
-        return playName;
-    }
-
-    public void setPlayName(String playName) {
-        this.playName = playName;
-    }
-
-    public String getPlayRace() {
-        return playRace;
-    }
-
-    public void setPlayRace(String playRace) {
-        this.playRace = playRace;
-    }
-
-    public Classes getPlayClass() {
-        return playClass;
-    }
-
-    public void setPlayClass(Classes playClass) {
-        this.playClass = playClass;
-    }
-
-    public int getDex() {
-        return dex;
-    }
-
-    public void setDex(int dex) {
-        this.dex = dex;
-    }
-
-    public int getStr() {
-        return str;
-    }
-
-    public void setStr(int str) {
-        this.str = str;
-    }
-
-    public int getCon() {
-        return con;
-    }
-
-    public void setCon(int con) {
-        this.con = con;
-    }
-
-    public int getIntel() {
-        return intel;
-    }
-
-    public void setIntel(int intel) {
-        this.intel = intel;
-    }
-
-    public int getWis() {
-        return wis;
-    }
-
-    public void setWis(int wis) {
-        this.wis = wis;
-    }
-
-    public int getCha() {
-        return cha;
-    }
-
-    public void setCha(int cha) {
-        this.cha = cha;
-    }
-
-    public playerChar(){
-
-    }
-
-    private void calcStats(){
-        ArrayList<Integer> stats = new ArrayList<>();
-        for (int i = 0; i < 6; i++) {
-            stats.add(sum3());
-        }
-//         dex = sum3();
-//         str = sum3();
-//         con = sum3();
-//         intel = sum3();
-//         wis = sum3();
-//         cha = sum3();
-    }
-
 }
