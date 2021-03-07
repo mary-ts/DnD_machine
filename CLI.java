44653f4822051c83cf492c31569656deac4e3a96
Mary Tease <58113541+mary-ts@users.noreply.github.com>
2021-03-07 19:26:09 +0000

    Update CLI.java

--- CLI.java
+++ CLI.java
@@ -47,7 +47,7 @@ public class CLI {
                         playRace = Character.Dwarf;
                         break;
                 }
-                System.out.println("Choose a Class")
+                System.out.println("Choose a Class");
                 System.out.println("1. Barbarian      2. Bard       3. Cleric     4. Druid");
                 System.out.println("5. Fighter        6. Monk       7. Paladin    8. Ranger");
                 System.out.println("9. Rogue          10. Sorcerer  11. Warlock   12. Wizard");
